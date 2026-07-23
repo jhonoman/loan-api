5239f430ca1a04da86ec650e6f4be5c8062d25b1
jhonoman <nagibzzz@gmail.com>
2026-07-23 15:00:11 +0700

    feat: add rejected loan API endpoint and update readme

---
+++ src/main/java/com/example/loanapi/dto/RejectLoanResponse.java
@@ -0,0 +1,19 @@
+package com.example.loanapi.dto;
+
+lombok.AllArgsConstructor;
+lombok.Builder;
+lombok.Getter;
+lombok.NoArgsConstructor;
+lombok.Setter;
+
+Setter
+Getter
+uilder
+@NoArgs#Constructor
+AllArgsConstructor
+public class RejectLoanResponse {
+    private String userId;
+    private String policeNumber;
+    private String status;
+    private String rejectionReason;
+}
